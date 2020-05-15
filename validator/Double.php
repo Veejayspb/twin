527129ac893830818516fb3763af7ca2a7a33c4c
Evgen <veejayspb@gmail.com>
2020-05-15 14:26:42 +0300

    Numeric validators must receive string type.

--- validator/Double.php
+++ validator/Double.php
@@ -8,12 +8,18 @@ class Double extends Numeric
      * Проверка типа.
      * @param mixed $value
      * @param string $label
+     * @param string $attribute
      * @return bool
      */
-    public function type($value, string $label): bool
+    public function type($value, string $label, string $attribute): bool
     {
         $this->message = "$label не является числом";
         $type = gettype($value);
-        return $type == 'integer' || $type == 'double';
+        if ($type == 'integer' || $type == 'double') return true;
+        if ($type == 'string' && preg_match('/^-?[0-9]+(\.[0-9]+)?$/', $value)) {
+            $this->model->$attribute = (double)$value;
+            return true;
+        }
+        return false;
     }
 }
--- validator/Integer.php
+++ validator/Integer.php
@@ -8,11 +8,18 @@ class Integer extends Numeric
      * Проверка типа.
      * @param mixed $value
      * @param string $label
+     * @param string $attribute
      * @return bool
      */
-    public function type($value, string $label): bool
+    public function type($value, string $label, string $attribute): bool
     {
         $this->message = "$label не является целым числом";
-        return 'integer' == gettype($value);
+        $type = gettype($value);
+        if ($type == 'integer') return true;
+        if ($type == 'string' && preg_match('/^-?[0-9]+$/', $value)) {
+            $this->model->$attribute = (int)$value;
+            return true;
+        }
+        return false;
     }
 }
