d328a383f1c7ca4063bda68c6fcec7b210988ea5
Evgen <veejayspb@gmail.com>
2020-06-04 19:46:20 +0300

    Change Bootstrap asset version to 3.

--- asset/collection/BootstrapAsset.php
+++ asset/collection/BootstrapAsset.php
@@ -11,14 +11,14 @@ class BootstrapAsset extends Asset
      * {@inheritdoc}
      */
     public $css = [
-        'main' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+        'main' => 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css',
     ];
 
     /**
      * {@inheritdoc}
      */
     public $js = [
-        'main' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js',
+        'main' => 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js',
     ];
 
     /**
@@ -34,7 +34,7 @@ class BootstrapAsset extends Asset
     protected function prepareCss(Tag $tag, $key): Tag
     {
         if ($key == 'main') {
-            $tag->integrity = 'sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk';
+            $tag->integrity = 'sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu';
             $tag->crossorigin = 'anonymous';
         }
         return $tag;
@@ -46,7 +46,7 @@ class BootstrapAsset extends Asset
     protected function prepareJs(Tag $tag, $key): Tag
     {
         if ($key == 'main') {
-            $tag->integrity = 'sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI';
+            $tag->integrity = 'sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd';
             $tag->crossorigin = 'anonymous';
         }
         return $tag;
