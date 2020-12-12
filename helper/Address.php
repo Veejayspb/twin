1fb9a5bc8d5d67e212f20b154fca2c2638a96b90
Evgen <veejayspb@gmail.com>
2020-12-12 20:38:44 +0300

    AddressBuilder.

--- helper/Address.php
+++ helper/Address.php
@@ -4,7 +4,6 @@ namespace twin\helper;
 
 /**
  * Class Address
- * @package core\helper
  *
  * @property bool $ssl
  * @property string|null $domain
@@ -24,13 +23,13 @@ class Address
      * Имя домена.
      * @var string|null
      */
-    private $domain;
+    private $domain = '';
 
     /**
      * Относительный путь.
      * @var string|null
      */
-    private $path;
+    private $path = '';
 
     /**
      * GET-параметры.
@@ -42,31 +41,29 @@ class Address
      * Якорь.
      * @var string|null
      */
-    private $anchor;
+    private $anchor = '';
 
     /**
-     * @param string|null $url - адрес
+     * @param string $url - адрес
      */
-    public function __construct(string $url = null)
+    public function __construct(string $url = '')
     {
-        if ($url !== null) {
-            $parts = parse_url($url);
-            if (isset($parts['scheme'])) {
-                $this->ssl = $parts['scheme'] == 'https';
-            }
-            if (isset($parts['host'])) {
-                $this->domain = $parts['host'];
-            }
-            if (isset($parts['path'])) {
-                $this->path = $parts['path'];
-            }
-            if (isset($parts['query'])) {
-                parse_str($parts['query'], $params);
-                $this->params = $params;
-            }
-            if (isset($parts['fragment'])) {
-                $this->anchor = $parts['fragment'];
-            }
+        $parts = parse_url($url);
+        if (isset($parts['scheme'])) {
+            $this->ssl = $parts['scheme'] == 'https';
+        }
+        if (isset($parts['host'])) {
+            $this->domain = $parts['host'];
+        }
+        if (isset($parts['path'])) {
+            $this->path = $parts['path'];
+        }
+        if (isset($parts['query'])) {
+            parse_str($parts['query'], $params);
+            $this->params = $params;
+        }
+        if (isset($parts['fragment'])) {
+            $this->anchor = $parts['fragment'];
         }
     }
 
@@ -76,14 +73,14 @@ class Address
      */
     public function __set($name, $value)
     {
-        if (!property_exists($this, $name)) return;
-
         if ($name == 'params' && is_array($value)) {
             $this->params = array_filter($value, function ($value) {
                 return $value !== null;
             });
-        } else {
-            $this->$name = $value;
+        } elseif ($name == 'ssl') {
+            $this->ssl = (bool)$value;
+        } elseif (property_exists($this, $name)) {
+            $this->$name = (string)$value;
         }
     }
 
@@ -118,5 +115,124 @@ class Address
             $url.= '#' . $this->anchor;
         }
         return $url;
+    }
+
+    /**
+     * Инстанс строителя адреса.
+     * @return AddressBuilder
+     */
+    public function build(): AddressBuilder
+    {
+        return new AddressBuilder($this);
+    }
+}
+
+/**
+ * Class AddressBuilder
+ *
+ * @method self scheme(bool $value) - указать наличие протокола
+ * @method self domain(bool $value) - указать наличие домена
+ * @method self path(bool $value) - указать наличие пути
+ * @method self params(bool $value) - указать наличие параметров
+ * @method self anchor(bool $value) - указать наличие якоря
+ */
+final class AddressBuilder
+{
+    /**
+     * Включенные в адрес части.
+     * @var array
+     */
+    private $parts = [
+        'scheme' => false,
+        'domain' => false,
+        'path' => true,
+        'params' => false,
+        'anchor' => false,
+    ];
+
+    /**
+     * Объект с адресом.
+     * @var Address
+     */
+    private $address;
+
+    /**
+     * @param Address $address
+     */
+    public function __construct(Address $address)
+    {
+        $this->address = $address;
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (array_key_exists($name, $this->parts) && !empty($arguments)) {
+            $this->parts[$name] = (bool)$arguments[0];
+        }
+        return $this;
+    }
+
+    /**
+     * Добавить протокол и домен.
+     * @param bool $value
+     * @return self
+     */
+    public function absolute(bool $value = true): self
+    {
+        return $this->scheme($value)->domain($value);
+    }
+
+    /**
+     * Сгенерировать адрес.
+     * @return string
+     */
+    public function get(): string
+    {
+        $this->fillParts();
+        $address = $this->address;
+
+        $url = '';
+        if ($this->parts['scheme'] && $address->domain) {
+            $url.= $address->ssl ? 'https://' : 'http://';
+        }
+        if ($this->parts['domain'] && $address->domain) {
+            $url.= $address->domain;
+        }
+        if ($this->parts['path']) {
+            $url.= $address->path;
+        }
+        if ($this->parts['params']) {
+            $url.= '?' . http_build_query($address->params, '', '&');
+        }
+        if ($this->parts['anchor']) {
+            $url.= '#' . $address->anchor;
+        }
+        return $url;
+    }
+
+    /**
+     * Включить в адрес все параметры, находящиеся между первым и последним входящими в адрес параметрами.
+     * @return void
+     */
+    private function fillParts()
+    {
+        // Определение первого и последнего входящих в адрес параметров.
+        $from = $to = null;
+        foreach ($this->parts as $name => $include) {
+            if ($include) {
+                $to = $name;
+                if ($from === null) {
+                    $from = $name;
+                }
+            }
+        }
+        // Включить параметры.
+        $started = false;
+        foreach ($this->parts as $name => $include) {
+            if ($name == $from) $started = true;
+            $this->parts[$name] = $started;
+            if ($name == $to) break;
+        }
+
     }
 }
