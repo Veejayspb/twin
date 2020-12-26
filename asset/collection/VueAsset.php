d3c87d191bb6593a4310e93513230a29fc74ed0e
Evgen <veejayspb@gmail.com>
2020-12-26 03:21:07 +0300

    Update VueAsset to v3.0.4

--- asset/collection/VueAsset.php
+++ asset/collection/VueAsset.php
@@ -10,6 +10,6 @@ class VueAsset extends Asset
      * {@inheritdoc}
      */
     public $js = [
-        'https://cdn.jsdelivr.net/npm/vue',
+        'https://unpkg.com/vue@3.0.4/dist/vue.global.js',
     ];
 }
