50c78a247135bc8770a1eebf06c0d77ffb7e2e28
Evgen <veejayspb@gmail.com>
2021-04-23 02:25:29 +0300

    Pagination.

---
+++ helper/Pagination.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace twin\helper;
+
+use twin\common\Exception;
+use twin\model\query\Query;
+use twin\widget\PaginationWidget;
+
+/**
+ * Class Pagination
+ * @property-read int $total
+ * @property-read int $page
+ * @property-read int $limit
+ * @property-read int $offset - отступ
+ * @property-read int $amount - кол-во страниц
+ */
+class Pagination
+{
+    /**
+     * Общее кол-во элементов.
+     * @var int
+     */
+    protected $total;
+
+    /**
+     * Номер страницы.
+     * @var int
+     */
+    protected $page = 1;
+
+    /**
+     * Лимит элементов на одну страницу.
+     * @var int
+     */
+    protected $limit = 10;
+
+    /**
+     * @param int $total
+     * @param int $page
+     * @param int $limit
+     */
+    public function __construct(int $total, int $page = 1, int $limit = 10)
+    {
+        $this->setTotal($total);
+        $this->setPage($page);
+        $this->setSize($limit);
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        if ($name == 'offset') {
+            return $this->getOffset();
+        } elseif ($name == 'amount') {
+            return $this->getAmount();
+        } else {
+            return $this->$name;
+        }
+    }
+
+    /**
+     * Установить общее кол-во записей.
+     * @param int $value
+     * @return static
+     */
+    public function setTotal(int $value): self
+    {
+        $this->total = $value <= 0 ? 0 : $value;
+        return $this;
+    }
+
+    /**
+     * Установить номер страницы.
+     * @param int $value
+     * @return static
+     */
+    public function setPage(int $value): self
+    {
+        $this->page = $value <= 1 ? 1 : $value;
+        return $this;
+    }
+
+    /**
+     * Установить размер страницы.
+     * @param int $value
+     * @return static
+     */
+    public function setSize(int $value): self
+    {
+        $this->limit = $value <= 1 ? 1 : $value;
+        return $this;
+    }
+
+    /**
+     * Применить лимит и отступ к выборке на базе ActiveModel.
+     * @param Query $query
+     * @return Query
+     */
+    public function apply(Query $query): Query
+    {
+        return $query
+            ->offset($this->offset)
+            ->limit($this->limit);
+    }
+
+    /**
+     * Виджет пагинатора.
+     * @param array $properties - установить свойства виджета
+     * @param string $class - класс виджета пагинатора
+     * @return string
+     * @throws Exception
+     */
+    public function widget(array $properties = [], string $class = PaginationWidget::class): string
+    {
+        if (!is_a($class, PaginationWidget::class, true)) {
+            throw new Exception(500, "$class must extends " . PaginationWidget::class);
+        }
+
+        $properties['pagination'] = $this;
+        $widget = new $class($properties); /* @var PaginationWidget $widget */
+        return $widget->run();
+    }
+
+    /**
+     * Вернуть отступ.
+     * @return int
+     */
+    protected function getOffset(): int
+    {
+        return $this->limit * ($this->page - 1);
+    }
+
+    /**
+     * Подсчитать кол-во страниц.
+     * @return int
+     */
+    protected function getAmount(): int
+    {
+        return ceil($this->total / $this->limit);
+    }
+}
---
+++ widget/PaginationWidget.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace twin\widget;
+
+use twin\common\Exception;
+use twin\helper\Html;
+use twin\helper\Pagination;
+use twin\helper\Url;
+
+class PaginationWidget extends Nav
+{
+    /**
+     * CSS-класс неактивного пункта меню.
+     * @var string
+     */
+    public $disabledClass = 'disabled';
+
+    /**
+     * Название GET-параметра со страницей.
+     * @var string
+     */
+    public $parameter = 'page';
+
+    /**
+     * Отображать кнопки предыдущая/следующая страница.
+     * @var bool
+     */
+    public $prevNext = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    public $htmlAttributes = [
+        'class' => 'pagination',
+    ];
+
+    /**
+     * @var Pagination
+     */
+    protected $pagination;
+
+    /**
+     * {@inheritdoc}
+     * @throws Exception
+     */
+    public function __construct(array $properties = [])
+    {
+        parent::__construct($properties);
+
+        if (!is_a($this->pagination, Pagination::class)) {
+            throw new Exception(500, self::class . ' - required properties not specified: pagination');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run(): string
+    {
+        // Если страница всего одна, то не выводить пагинатор.
+        if ($this->pagination->amount == 1) {
+            return '';
+        }
+
+        $items = [];
+
+        if ($this->prevNext) {
+            $items[] = $this->prev();
+        }
+
+        $items = array_merge($items, $this->getNumbers());
+
+        if ($this->prevNext) {
+            $items[] = $this->next();
+        }
+
+        return Html::tag('ul', $this->htmlAttributes, implode(PHP_EOL, $items));
+    }
+
+    /**
+     * Номера страниц.
+     * @return array
+     */
+    protected function getNumbers(): array
+    {
+        $current = $this->pagination->page; // Номер текущей страницы
+        $total = $this->pagination->amount; // Кол-во страниц
+        $from = $current - 2;
+        $to = $current + 2;
+
+        $result = [];
+        for ($i = $from; $i <= $to && $i <= $total; $i++) {
+
+            $disabled = $i == $current;
+
+            $result[] = $this->getItem([
+                'label' => $i,
+                'url' => $disabled ? '#' : Url::current([$this->parameter => $i]),
+                'visible' => 0 < $i,
+                'active' => $current == $i,
+            ]);
+        }
+        return $result;
+    }
+
+    /**
+     * Предыдущая страница.
+     * @return NavItem
+     */
+    protected function prev(): NavItem
+    {
+        $page = $this->pagination->page - 1;
+        $disabled = $page <= 0;
+
+        return $this->getItem([
+            'label' => '&laquo;',
+            'url' => $disabled ? '#' : Url::current([$this->parameter => $page]),
+            'active' => false,
+            'htmlAttributes' => ['class' => $disabled ? $this->disabledClass : false],
+        ]);
+    }
+
+    /**
+     * Следующая страница.
+     * @return NavItem
+     */
+    protected function next(): NavItem
+    {
+        $page = $this->pagination->page + 1;
+        $disabled = $this->pagination->amount < $page;
+
+        return $this->getItem([
+            'label' => '&raquo;',
+            'url' => $disabled ? '#' : Url::current([$this->parameter => $page]),
+            'active' => false,
+            'htmlAttributes' => ['class' => $disabled ? $this->disabledClass : false],
+        ]);
+    }
+}
