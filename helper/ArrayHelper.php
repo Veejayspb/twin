e2d9b085b9e8d659daeabd4da3699f5a53e0796a
Evgen <veejayspb@gmail.com>
2023-01-16 10:26:10 +0300

    Refactor ArrayHelper class.

--- helper/ArrayHelper.php
+++ helper/ArrayHelper.php
@@ -7,18 +7,21 @@ class ArrayHelper
     /**
      * Формирование нового массива на базе исходного.
      * @param array $array - исходный массив
-     * @param callable $valueCallback - коллбэк для формирования значения: function($value, $key) {...}
-     * @param callable|null $keyCallback - коллбэк для формирования ключа. Если NULL, то будет взят исходный ключ.
+     * @param callable $keyCallback - коллбэк для формирования ключа: function($key, $value) {...}
+     * @param callable $valueCallback - коллбэк для формирования значения: function($key, $value) {...}
      * @return array
      */
-    public static function column(array $array, callable $valueCallback, callable $keyCallback = null): array
+    public static function column(array $array, callable $keyCallback, callable $valueCallback): array
     {
         $result = [];
+
         foreach ($array as $key => $value) {
-            $k = $keyCallback === null ? $key : $keyCallback($value, $key);
-            $v = $valueCallback($value, $key);
+            $k = $keyCallback($key, $value);
+            $v = $valueCallback($key, $value);
+
             $result[$k] = $v;
         }
+
         return $result;
     }
 
@@ -32,10 +35,12 @@ class ArrayHelper
     {
         foreach ($items as $i => $item) {
             $item = (array)$item;
+
             foreach ($search as $key => $value) {
                 if (!isset($item[$key])) continue 2;
                 if ($value !== $item[$key]) continue 2;
             }
+
             return $i;
         }
         return false;
@@ -66,18 +71,13 @@ class ArrayHelper
     public static function merge(array $array_1, array $array_2): array
     {
         foreach ($array_2 as $key => $value) {
-            // Если ключ в 1 массиве отсутствует, то сразу заполняем его
-            if (!array_key_exists($key, $array_1)) {
+            if (
+                !array_key_exists($key, $array_1) ||
+                !is_array($array_1[$key]) ||
+                !is_array($value)
+            ) {
                 $array_1[$key] = $value;
-            }
-
-            // Замена значения
-            if (!is_array($array_1[$key]) && !is_array($value)) {
-                $array_1[$key] = $value;
-            }
-
-            // Если значения из 1 и 2 массивов, совпадающие по ключам являются массивами, то продолжить рекурсивное слияние.
-            if (is_array($array_1[$key]) && is_array($value)) {
+            } else {
                 $array_1[$key] = static::merge($array_1[$key], $array_2[$key]);
             }
         }
