5ab3534033e6d6ce8f51d424862c9cabb7ec066a
Evgen <veejayspb@gmail.com>
2023-01-18 02:03:58 +0300

    Refactor Rule class.

--- route/Rule.php
+++ route/Rule.php
@@ -26,17 +26,23 @@ class Rule implements RuleInterface
         // Убрать из адреса GET-параметры.
         $address = new Address($url);
         $url = $address->build()->path()->get();
-
         $placeholders = $this->extractPlaceholders($url);
-        if ($placeholders === false) return false;
+
+        if ($placeholders === false) {
+            return false;
+        }
 
         $strRoute = $this->fillRoute($placeholders); // Строковый роут вида: module/controller/action или controller/action
-        if (!preg_match('/^([a-z]+\/)?[a-z]+\/[a-z]+$/', $strRoute)) return false;
+
+        if (!preg_match('/^([a-z]+\/)?[a-z]+\/[a-z]+$/', $strRoute)) {
+            return false;
+        }
 
         $route = new Route;
         $route->setRoute($strRoute);
         $route->params = $address->params;
         $route->setProperties($placeholders);
+
         return $route;
     }
 
@@ -51,7 +57,10 @@ class Rule implements RuleInterface
         // Проверка на соответствие строковому роуту
         $reserved = $route->getReservedParams();
         $strRoute = $this->fillRoute($reserved);
-        if ($strRoute != $route->getRoute()) return false;
+
+        if ($strRoute != $route->getRoute()) {
+            return false;
+        }
 
         // Замена зарезервированных параметров
         foreach ($reserved as $name => $value) {
@@ -60,8 +69,10 @@ class Rule implements RuleInterface
 
         // Замена параметров
         $params = [];
+
         foreach ($route->params as $name => $value) {
             $replacement = $this->replacePlaceholder($url, $name, $value);
+
             if (!$replacement) {
                 $params[$name] = $value;
             }
@@ -71,6 +82,7 @@ class Rule implements RuleInterface
 
         $address->path = $url;
         $address->params = $params;
+
         return $address->build()->path()->params()->anchor()->get();
     }
 
@@ -85,12 +97,15 @@ class Rule implements RuleInterface
     private function replacePlaceholder(string &$str, string $name, $value): bool
     {
         $str = preg_replace_callback("/<$name(:(.+?))?>/", function ($m) use ($value) {
+
             if (!array_key_exists(2, $m) || preg_match('/^' . $m[2] . '$/', $value)) {
                 return $value;
             } else {
                 return $m[0];
             }
+
         }, $str, -1, $count);
+
         return 0 < $count;
     }
 
@@ -101,8 +116,7 @@ class Rule implements RuleInterface
      */
     private function hasPlaceholders(string $str): bool
     {
-        $pattern = '/<.+?>/';
-        return preg_match($pattern, $str);
+        return preg_match('/<.+?>/', $str);
     }
 
     /**
@@ -113,9 +127,11 @@ class Rule implements RuleInterface
     private function fillRoute(array $params): string
     {
         $route = $this->route;
+
         foreach ($params as $name => $value) {
             $route = str_replace("<$name>", $value, $route);
         }
+
         return $route;
     }
 
@@ -129,19 +145,23 @@ class Rule implements RuleInterface
         $pattern = str_replace('/', '\/', $this->pattern);
 
         // Создать регулярное выражение с именованными параметрами.
-        $pattern = preg_replace_callback('/<(.+?)(:(.+?))?\>/', function ($m) {
-            $range = isset($m[3]) ? $m[3] : '.+?';
+        $pattern = preg_replace_callback('/<(.+?)(:(.+?))?>/', function ($m) {
+            $range = $m[3] ?? '.+?';
             $name = $m[1];
             return "(?P<$name>$range)";
         }, $pattern);
 
-        if (!preg_match_all("/^$pattern$/", $url, $m)) return false;
+        if (!preg_match_all("/^$pattern$/", $url, $m)) {
+            return false;
+        }
 
         $result = [];
+
         foreach ($m as $key => $value) {
             if (is_int($key)) continue;
             $result[$key] = $value[0];
         }
+
         return $result;
     }
 }
