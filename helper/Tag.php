b0b15b6b8ef797fdb03d0c65574d034d727b5664
Evgen <veejayspb@gmail.com>
2023-08-04 01:23:55 +0300

    [refactor]: Tag class.

--- helper/Tag.php
+++ helper/Tag.php
@@ -4,12 +4,6 @@ namespace twin\helper;
 
 /**
  * Хелпер для формирования HTML-тегов.
- * Атрибуты тега могут задаваться и считываться напрямую:
- * $tag->attr = 'value';
- * $value = $tag->attr;
- * Если атрибут не сущ-ет, то вернется NULL.
- * Чтобы удалить атрибут, необходимо выставить его значение в NULL или FALSE.
- * В кач-ве значения атрибута может выступать массив. Результатом будет склейка всех значений через пробел.
  *
  * Class Tag
  */
@@ -17,9 +11,8 @@ class Tag
 {
     /**
      * Одиночные теги.
-     * @var array
      */
-    public static $singleTags = [
+    const SINGLE_TAGS = [
         'area',
         'base',
         'br',
@@ -37,18 +30,22 @@ class Tag
         'track',
         'wbr',
     ];
+
+    /**
+     * Название тега по-умолчанию.
+     */
+    const DEFAULT_NAME = 'undefined';
+
+    /**
+     * Паттерн названия атрибута тега.
+     */
+    const ATTRIBUTE_NAME_PATTERN = '/^[a-z\-]+$/';
     
     /**
      * Название тега.
      * @var string
      */
     protected $name;
-
-    /**
-     * Является ли тег одиночным.
-     * @var bool
-     */
-    protected $single = false;
 
     /**
      * Содержимое тега.
@@ -64,37 +61,14 @@ class Tag
 
     /**
      * @param string $name - название тега
-     * @param array $htmlAttributes - атрибуты тега
+     * @param array $attributes - атрибуты тега
      * @param string $content - содержимое тега
-     * @param bool|null $single - является ли тег одиночным (если NULL, то определяется автоматически)
      */
-    public function __construct(string $name, array $htmlAttributes = [], string $content = '', bool $single = null)
+    public function __construct(string $name, array $attributes = [], string $content = '')
     {
-        $this->name = $name;
-        $this->attributes = $htmlAttributes;
-        $this->content = $content;
-        $this->single = $single === null ? in_array($name, static::$singleTags) : $single;
-    }
-
-    /**
-     * @param string $name - название атрибута
-     * @param string|array|null $value - значение
-     */
-    public function __set($name, $value)
-    {
-        $this->attributes[$name] = $value;
-        if ($value === false || $value === null) {
-            unset($this->attributes[$name]);
-        }
-    }
-
-    /**
-     * @param string $name - название атрибута
-     * @return mixed|null
-     */
-    public function __get($name)
-    {
-        return array_key_exists($name, $this->attributes) ? $this->attributes[$name] : null;
+        $this->setName($name);
+        $this->setAttributes($attributes);
+        $this->setContent($content);
     }
 
     /**
@@ -103,10 +77,12 @@ class Tag
     public function __toString()
     {
         $result = $this->open();
-        if (!$this->single) {
+
+        if (!$this->isSingle()) {
             $result.= $this->content;
             $result.= $this->close();
         }
+
         return $result;
     }
 
@@ -116,7 +92,12 @@ class Tag
      */
     public function open(): string
     {
-        $attributes = static::renderAttributes();
+        $attributes = $this->renderAttributes();
+
+        if (!empty($attributes)) {
+            $attributes = ' ' . $attributes;
+        }
+
         return "<$this->name$attributes>";
     }
 
@@ -130,23 +111,168 @@ class Tag
     }
 
     /**
+     * Указать название тега.
+     * @param string $value
+     * @return void
+     */
+    public function setName(string $value): void
+    {
+        $this->name = $value ?: static::DEFAULT_NAME;
+    }
+
+    /**
+     * Вернуть название тега.
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Указать значение атрибута тега.
+     * Чтобы удалить атрибут, необходимо выставить его значение в NULL или FALSE.
+     * @param mixed $name - название атрибута
+     * @param mixed $value - значение атрибута
+     * @return bool
+     */
+    public function setAttribute($name, $value): bool
+    {
+        if (!$this->isValidAttributeName($name) || !$this->isValidAttributeValue($value)) {
+            return false;
+        }
+
+        $this->attributes[$name] = $value;
+
+        if ($this->isEmptyAttributeValue($value)) {
+            unset($this->attributes[$name]);
+        }
+
+        return true;
+    }
+
+    /**
+     * Указать значения атрибутов тега.
+     * @param array $attributes
+     * @return void
+     */
+    public function setAttributes(array $attributes): void
+    {
+        $this->attributes = [];
+
+        foreach ($attributes as $name => $value) {
+            $this->setAttribute($name, $value);
+        }
+    }
+
+    /**
+     * Вернуть значение атрибута тега.
+     * @param string $name
+     * @return mixed|null
+     */
+    public function getAttribute(string $name)
+    {
+        return array_key_exists($name, $this->attributes) ? $this->attributes[$name] : null;
+    }
+
+    /**
+     * Вернуть атрибуты тега.
+     * @return array
+     */
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Указать содержимое тега.
+     * @param string $value
+     * @return void
+     */
+    public function setContent(string $value): void
+    {
+        $this->content = $value;
+    }
+
+    /**
+     * Вернуть содержимое тега.
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * Является ли тег одиночным.
+     * @return bool
+     */
+    public function isSingle(): bool
+    {
+        return in_array($this->name, static::SINGLE_TAGS);
+    }
+
+    /**
+     * Является ли значение атрибута пустым.
+     * @param mixed $value
+     * @return bool
+     */
+    protected function isEmptyAttributeValue($value): bool
+    {
+        return in_array($value, [null, false], true);
+    }
+
+    /**
+     * Является ли название атрибута корректным.
+     * @param mixed $name
+     * @return bool
+     */
+    protected function isValidAttributeName($name): bool
+    {
+        return
+            is_string($name) &&
+            preg_match(static::ATTRIBUTE_NAME_PATTERN, $name);
+    }
+
+    /**
+     * Является ли значение атрибута корректным.
+     * @param mixed $value
+     * @return bool
+     */
+    protected function isValidAttributeValue($value): bool
+    {
+        $type = gettype($value);
+        return in_array($type, ['string', 'integer', 'boolean', 'NULL']);
+    }
+
+    /**
      * Сформировать строку с атрибутами для тега.
      * @return string
      */
     protected function renderAttributes(): string
     {
-        $result = '';
+        $result = [];
+
         foreach ($this->attributes as $key => $value) {
-            if ($value === true) {
-                $result.= " $key";
-            } elseif ($value !== false && $value !== null) {
-                if (is_array($value)) {
-                    $value = implode(' ', $value);
-                }
-                $value = htmlentities($value, ENT_QUOTES);
-                $result.= " $key=\"$value\"";
-            }
+            $result[] = $this->renderAttribute($key, $value);
         }
-        return $result;
+
+        return implode(' ', $result);
+    }
+
+    /**
+     * Сформировать строковый атрибут тега, имея ключ и значение.
+     * @param string $key
+     * @param true|string|array $value
+     * @return string
+     */
+    private function renderAttribute(string $key, $value): string
+    {
+        if ($value === true) {
+            return $key;
+        }
+
+        $value = htmlentities((string)$value, ENT_QUOTES);
+        return "$key=\"$value\"";
     }
 }
