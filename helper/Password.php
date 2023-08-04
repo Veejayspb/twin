6c12c5f582d965e64681bed6ed0e2f84dbe1dd07
Evgen <veejayspb@gmail.com>
2023-08-04 14:57:36 +0300

    [refactor]: Password class.

--- helper/Password.php
+++ helper/Password.php
@@ -2,8 +2,6 @@
 
 namespace twin\helper;
 
-use twin\common\Exception;
-
 class Password
 {
     /**
@@ -14,8 +12,7 @@ class Password
      */
     public static function hash(string $password, int $cost = 13): string
     {
-        $salt = static::generateSalt($cost);
-        return crypt($password, $salt);
+        return password_hash($password, PASSWORD_DEFAULT, ['cost' => $cost]);
     }
 
     /**
@@ -26,56 +23,6 @@ class Password
      */
     public static function check(string $password, string $hash): bool
     {
-        if ($password == '') {
-            return false;
-        }
-
-        $test = crypt($password, $hash);
-
-        if (!is_string($test) || strlen($test) < 32) {
-            return false;
-        }
-
-        return self::compare($test, $hash);
-    }
-
-    /**
-     * Сгенерировать соль.
-     * @param int $cost - значение (4-31), используемое алгоритмом Blowfish
-     * @return string
-     * @throws Exception
-     */
-    private static function generateSalt(int $cost = 13): string
-    {
-        if ($cost < 4 || 31 < $cost) {
-            throw new Exception(500, 'Cost value must be between 4 and 31');
-        }
-
-        $randomBase64 = (new RandomString)->useUpper()->run(22, 22);
-        return '$2a$' . $cost . '$' . $randomBase64;
-    }
-
-    /**
-     * Сравнить пару хэшированных паролей.
-     * @param string $a - первая строка с хэшем
-     * @param string $b - вторая строка с хэшем
-     * @return bool
-     */
-    private static function compare(string $a, string $b): bool
-    {
-        $mb = function_exists('mb_strlen');
-        $length = $mb ? mb_strlen($a, '8bit') : strlen($a);
-
-        if ($length !== ($mb ? mb_strlen($b, '8bit') : strlen($b))) {
-            return false;
-        }
-
-        $check = 0;
-
-        for($i = 0; $i < $length; $i++) {
-            $check |= (ord($a[$i]) ^ ord($b[$i]));
-        }
-
-        return $check === 0;
+        return password_verify($password, $hash);
     }
 }
