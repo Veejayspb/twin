55a68d3cb47edd1c9369c4e01dc23731cb96e100
Evgen <veejayspb@gmail.com>
2023-08-05 05:04:03 +0300

    [refactor]: ObjectHelper class.

--- helper/ObjectHelper.php
+++ helper/ObjectHelper.php
@@ -2,27 +2,21 @@
 
 namespace twin\helper;
 
-use twin\common\Exception;
-
 class ObjectHelper
 {
     /**
      * Заполнить свойства объекта.
-     * @param mixed $object - исходный объект
+     * @param object $object - исходный объект
      * @param array $properties - публичные нестатические свойства
-     * @return mixed
-     * @throws Exception
+     * @return object
      */
-    public static function setProperties($object, array $properties)
+    public static function setProperties(object $object, array $properties): object
     {
-        if (!is_object($object)) {
-            throw new Exception(500, 'Wrong param type: ' . gettype($object));
-        }
-
         foreach ($properties as $name => $value) {
             if (!property_exists($object, $name)) {
                 continue;
             }
+
             $object->$name = $value;
         }
 
