73e2d9b824aae0bcc62d20c5e2ea08bba2375e2d
Evgen <veejayspb@gmail.com>
2023-08-05 05:05:02 +0300

    [test]: ObjectHelperTest.

---
+++ test/unit/helper/ObjectHelperTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace twin\test\unit\helper;
+
+use twin\helper\ObjectHelper;
+use twin\test\helper\BaseTestCase;
+
+class ObjectHelperTest extends BaseTestCase
+{
+    public function testSetProperties()
+    {
+        $object = new class {
+            public $a;
+            public $b;
+            public static $c;
+        };
+
+        ObjectHelper::setProperties($object, ['b' => 1, 'c' => 2, 'd' => 3]);
+
+        $this->assertNull($object->a);
+        $this->assertSame(1, $object->b);
+        $this->assertNull($object::$c);
+        $this->assertFalse(property_exists($object, 'd'));
+    }
+}
