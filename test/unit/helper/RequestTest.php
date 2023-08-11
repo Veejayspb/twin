b387d0e9ff9b7ff7eb3ef46e3e07646e92929ae2
Evgen <veejayspb@gmail.com>
2023-08-11 22:17:46 +0300

    [test]: RequestTest.

---
+++ test/unit/helper/RequestTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use twin\helper\Request;
+use twin\test\helper\BaseTestCase;
+
+class RequestTest extends BaseTestCase
+{
+    public function testIsAjax()
+    {
+        $this->assertFalse(Request::isAjax());
+
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+        $this->assertTrue(Request::isAjax());
+
+        $_SERVER['HTTP_X_REQUESTED_WITH'] = 'InvalidType';
+        $this->assertFalse(Request::isAjax());
+    }
+
+    public function testGet()
+    {
+        $this->assertNull(Request::get('param'));
+        $this->assertSame(111, Request::get('param', 111));
+
+        $_GET['param'] = 'val';
+        $this->assertSame('val', Request::get('param'));
+    }
+
+    public function testPost()
+    {
+        $this->assertNull(Request::post('param'));
+        $this->assertSame([222], Request::post('param', [222]));
+
+        $_POST['param'] = 'val';
+        $this->assertSame('val', Request::post('param'));
+    }
+
+    public function testRequest()
+    {
+        $this->assertNull(Request::request('param'));
+        $this->assertSame([444], Request::request('param', [444]));
+
+        $_REQUEST['param'] = 'val';
+        $this->assertSame('val', Request::request('param'));
+    }
+
+    public function testFiles()
+    {
+        $_FILES = [
+            'Model' => [
+                'name' => '...',
+                'type' => '...',
+                'tmp_name' => '...',
+                'error' => '...',
+                'size' => '...',
+            ],
+        ];
+
+        $this->assertSame([], Request::files('notexists'));
+        $this->assertSame($_FILES['Model'], Request::files('Model'));
+    }
+}
