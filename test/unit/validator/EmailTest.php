ce3d35b8dca3063ef476cc4ccae90e374ad26ac7
Evgen <veejayspb@gmail.com>
2023-08-25 01:03:23 +0300

    [test]: EmailTest.

---
+++ test/unit/validator/EmailTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use twin\test\helper\BaseTestCase;
+use twin\test\helper\TempModel;
+use twin\validator\Email;
+
+final class EmailTest extends BaseTestCase
+{
+    public function testEmail()
+    {
+        $items = [
+            [
+                'value' => 'name01@gmail.com',
+                'expected' => true,
+            ],
+            [
+                'value' => 'name@sub.dom-ain.info',
+                'expected' => true,
+            ],
+            [
+                'value' => 'ва-ся@домен.рф',
+                'expected' => true,
+            ],
+            [
+                'value' => '-name@domain.ru',
+                'expected' => false,
+            ],
+            [
+                'value' => '@sub.domain.com',
+                'expected' => false,
+            ],
+            [
+                'value' => 'name@',
+                'expected' => false,
+            ],
+            [
+                'value' => 'name@domain',
+                'expected' => false,
+            ],
+            [
+                'value' => 'name',
+                'expected' => false,
+            ],
+            [
+                'value' => '',
+                'expected' => false,
+            ],
+        ];
+
+        $model = new TempModel;
+        $validator = new Email($model, ['a']);
+
+        foreach ($items as $item) {
+            $this->assertSame(
+                $item['expected'],
+                $validator->email($item['value'], 'a')
+            );
+        }
+    }
+}
