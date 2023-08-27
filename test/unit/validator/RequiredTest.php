bbea2056561b3bd8637722d37a7a889d93cbef0b
Evgen <veejayspb@gmail.com>
2023-08-27 17:40:41 +0300

    [test]: RequiredTest.

---
+++ test/unit/validator/RequiredTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use twin\test\helper\BaseTestCase;
+use twin\test\helper\TempModel;
+use twin\validator\Required;
+
+final class RequiredTest extends BaseTestCase
+{
+    public function testNotEmpty()
+    {
+        $items = [
+            [
+                'value' => null,
+                'expected' => false,
+            ],
+            [
+                'value' => '',
+                'expected' => false,
+            ],
+            [
+                'value' => ' ',
+                'expected' => true,
+            ],
+            [
+                'value' => [],
+                'expected' => true,
+            ],
+            [
+                'value' => 0,
+                'expected' => true,
+            ],
+            [
+                'value' => true,
+                'expected' => true,
+            ],
+            [
+                'value' => false,
+                'expected' => true,
+            ],
+        ];
+
+        foreach ($items as $item) {
+            $model = new TempModel;
+            $model->a = $item['value'];
+
+            $validator = new Required($model, ['a']);
+
+            $this->assertSame(
+                $item['expected'],
+                $validator->notEmpty('a')
+            );
+        }
+    }
+}
