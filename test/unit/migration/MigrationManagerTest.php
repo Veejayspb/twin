62d6d8ee015d00359d0c72a376b0e7cb9f5175d8
Evgen <veejayspb@gmail.com>
2023-10-08 01:05:43 +0300

    [test]: Tests for migration system.

---
+++ test/unit/migration/MigrationManagerTest.php
@@ -0,0 +1,182 @@
+<?php
+
+use PHPUnit\Framework\MockObject\MockObject;
+use twin\migration\Migration;
+use twin\migration\MigrationManager;
+use twin\test\helper\BaseTestCase;
+use twin\test\helper\Temp;
+use twin\Twin;
+
+final class MigrationManagerTest extends BaseTestCase
+{
+    /**
+     * Массив значений для генерации списка миграций.
+     * ключ - название класса миграции
+     * значение - значение, возвращаемое методом isApplied() (true/false)
+     */
+    const MIGRATIONS = [
+        'm_231006_000000_first' => false,
+        'm_231007_000000_second' => true,
+        'm_231008_000000_third' => false,
+        'm_231009_000000_231010_000000' => true,
+        'm_231010_000000_fifth' => true,
+    ];
+
+    public function testCreate()
+    {
+        $items = [
+            'name' => true, // Корректное название
+            'na.me' => false, // Не соответствует шаблону
+            'na|me' => false, // Недопустимый символ
+        ];
+
+        $manager = new MigrationManager([
+            'alias' => '@twin/test/temp',
+        ]);
+
+        foreach ($items as $name => $expected) {
+            $actual = $manager->create($name);
+            $this->assertSame($expected, $actual);
+        }
+    }
+
+    public function testGetMigrations()
+    {
+        $manager = new MigrationManager([
+            'alias' => '@twin/test/temp',
+        ]);
+        $count = 3;
+
+        // Создание миграций
+        for ($i = 1; $i <= $count; $i++) {
+            $manager->create('name' . $i);
+        }
+
+        $migrations = $manager->getMigrations();
+        $this->assertSame($count + 1, count($migrations));
+
+        // Проверка наличия всех миграций в массиве
+        foreach ($migrations as $i => $migration) {
+            if ($i === 0) {
+                $this->assertSame(m_000000_000000_init::class, $migration->getClass());
+            } else {
+                $this->assertSame('name' . $i, $migration->getName());
+            }
+        }
+    }
+
+    public function testGetNotAppliedMigrations()
+    {
+        $manager = $this->getMigrationManager();
+        $notApplied = $manager->getNotAppliedMigrations();
+
+        $this->assertSame(2, count($notApplied));
+        $this->assertArrayHasKey(0, $notApplied);
+        $this->assertArrayHasKey(1, $notApplied);
+        $this->assertSame('first', $notApplied[0]->getName());
+        $this->assertSame('third', $notApplied[1]->getName());
+    }
+
+    public function testGetLastMigration()
+    {
+        $manager = $this->getMigrationManager();
+        $migration = $manager->getLastMigration();
+
+        $this->assertNotNull($migration);
+        $this->assertSame('fifth', $migration->getName());
+    }
+
+    public function testFindMigration()
+    {
+        $manager = $this->getMigrationManager();
+
+        $migration = $manager->findMigration('notexists');
+        $this->assertNull($migration);
+
+        $migration = $manager->findMigration('m_231006_000000_first');
+        $this->assertNotNull($migration);
+        $this->assertSame('first', $migration->getName());
+
+        $migration = $manager->findMigration('second');
+        $this->assertNotNull($migration);
+        $this->assertSame('second', $migration->getName());
+
+        $migration = $manager->findMigration('231008_000000');
+        $this->assertNotNull($migration);
+        $this->assertSame('third', $migration->getName());
+
+        // Есть 2 миграции с данным названием: в дате и в имени
+        $migration = $manager->findMigration('231010_000000');
+        $this->assertNull($migration);
+    }
+
+    /**
+     * @return MockObject|MigrationManager
+     */
+    protected function getMigrationManager(): MigrationManager
+    {
+        $properties = [
+            'alias' => '@twin/test/temp',
+        ];
+
+        $manager = $this->getMockBuilder(MigrationManager::class)
+            ->setConstructorArgs([$properties])
+            ->onlyMethods(['getMigrations'])
+            ->getMock();
+
+        Twin::import('@twin/migration/m_000000_000000_init.php', true);
+        $migrations[] = new m_000000_000000_init($manager);
+
+        foreach (self::MIGRATIONS as $className => $isApplied) {
+            $migrations[] = $this->getMigration($className, $isApplied);
+        }
+
+        $manager
+            ->expects($this->any())
+            ->method('getMigrations')
+            ->willReturn($migrations);
+
+        return $manager;
+    }
+
+    /**
+     * @param string $class
+     * @param bool $isApplied
+     * @return MockObject|Migration
+     */
+    protected function getMigration(string $class, bool $isApplied): Migration
+    {
+        $mock = $this->getMockBuilder(Migration::class)
+            ->disableOriginalConstructor()
+            ->setMockClassName($class)
+            ->onlyMethods(['isApplied'])
+            ->getMockForAbstractClass();
+
+        $mock
+            ->expects($this->any())
+            ->method('isApplied')
+            ->willReturn($isApplied);
+
+        return $mock;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        (new Temp)->clear();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        (new Temp)->clear();
+    }
+}
---
+++ test/unit/migration/MigrationTest.php
@@ -0,0 +1,237 @@
+<?php
+
+use twin\db\Database;
+use twin\helper\Alias;
+use twin\migration\Migration;
+use twin\migration\MigrationManager;
+use twin\test\helper\BaseTestCase;
+use twin\test\helper\ObjectProxy;
+use twin\test\helper\Temp;
+
+final class MigrationTest extends BaseTestCase
+{
+    /**
+     * Корректное название класса миграции.
+     */
+    const CLASS_NAME = 'm_231007_040500_name';
+
+    public function testConstruct()
+    {
+        $items = [
+            'm_230918_165900_test' => 0,
+            'm_230918_165900_0000' => 0,
+            'm_000000_000000_test_name' => 0,
+            'qqq' => 500,
+            't_000000_000000_test' => 500,
+            'm_000000_000000_Test' => 500,
+            'm_2309_165900_test' => 500,
+            'm_230918_1659_test' => 500,
+            'm_230918_165900_' => 500,
+            'm_230918_165900' => 500,
+        ];
+
+        foreach ($items as $className => $expectedCode) {
+            $code = $this->catchExceptionCode(function () use ($className) {
+                $this->getMigration($className);
+            });
+
+            $this->assertSame($expectedCode, $code);
+        }
+    }
+
+    public function testGetManager()
+    {
+        $migration = $this->getMigration(self::CLASS_NAME);
+        $proxy = new ObjectProxy($migration);
+
+        $this->assertSame($proxy->manager, $migration->getManager());
+    }
+
+    public function testClass()
+    {
+        $migration = $this->getMigration(self::CLASS_NAME);
+        $this->assertSame(self::CLASS_NAME, $migration->getClass());
+    }
+
+    public function testGetName()
+    {
+        $items = [
+            'm_230919_135301_name' => 'name',
+            'm_230919_162310_test_name' => 'test_name',
+            'm_230919_162310_1' => '1',
+        ];
+
+        foreach ($items as $className => $name) {
+            $migration = $this->getMigration($className);
+            $this->assertSame($name, $migration->getName());
+        }
+    }
+
+    public function testGetDate()
+    {
+        $items = [
+            'm_230919_165614_test' => '2023-09-19 16:56:14',
+            'm_230101_000000_test' => '2023-01-01 00:00:00',
+        ];
+
+        foreach ($items as $className => $date) {
+            $migration = $this->getMigration($className);
+            $dateTime = DateTime::createFromFormat('Y-m-d H:i:s', $date);
+            $this->assertEquals($dateTime, $migration->getDate());
+        }
+    }
+
+    public function testGetHash()
+    {
+        $migration = $this->getMigration(self::CLASS_NAME);
+
+        // В любой момент времени хэш должен быть одним и тем же
+        $firstHash = $migration->getHash();
+        sleep(2);
+        $secondHash = $migration->getHash();
+
+        $this->assertSame(32, strlen($firstHash));
+        $this->assertSame($firstHash, $secondHash);
+    }
+
+    public function testIsApplied()
+    {
+        foreach ([true, false] as $expected) {
+            $db = $this->getDatabase([
+                'isMigrationApplied' => $expected,
+            ]);
+
+            $migration = $this->getMigration(self::CLASS_NAME, [
+                'getDb' => $db,
+            ]);
+
+            $this->assertSame($expected, $migration->isApplied());
+        }
+    }
+
+    public function testApply()
+    {
+        foreach ([true, false] as $expected) {
+            $db = $this->getDatabase([
+                'addMigration' => $expected,
+            ]);
+
+            $migration = $this->getMigration(self::CLASS_NAME, [
+                'getDb' => $db,
+            ]);
+
+            $this->assertSame($expected, $migration->apply());
+        }
+    }
+
+    public function testCancel()
+    {
+        foreach ([true, false] as $expected) {
+            $db = $this->getDatabase([
+                'deleteMigration' => $expected,
+            ]);
+
+            $migration = $this->getMigration(self::CLASS_NAME, [
+                'getDb' => $db,
+            ]);
+
+            $this->assertSame($expected, $migration->cancel());
+        }
+    }
+
+    public function testCreate()
+    {
+        $items = [
+            'name' => true, // Корректное название
+            'na.me' => false, // Не соответствует шаблону
+            'na|me' => false, // Недопустимый символ
+        ];
+
+        $temp = new Temp;
+        $alias = '@twin/test/temp';
+        $manager = $this->getMigrationManager();
+
+        foreach ($items as $name => $expected) {
+            $actual = Migration::create($alias, $name);
+            $this->assertSame($expected, $actual);
+
+            if ($actual === false) {
+                continue;
+            }
+
+            $migration = $manager->getLastMigration();
+            $className = $migration->getClass();
+
+            $this->assertFileContains(
+                Alias::get("$alias/$className.php"),
+                "class $className extends Migration"
+            );
+
+            $temp->clear();
+        }
+    }
+
+    /**
+     * @param array $methods
+     * @return Database
+     */
+    protected function getDatabase(array $methods = []): Database
+    {
+        $mock = $this->getMockBuilder(Database::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(array_keys($methods))
+            ->getMockForAbstractClass();
+
+        foreach ($methods as $name => $value) {
+            $mock
+                ->expects($this->any())
+                ->method($name)
+                ->willReturn($value);
+        }
+
+        return $mock;
+    }
+
+    /**
+     * @param string $class
+     * @param array $methods
+     * @return Migration
+     */
+    protected function getMigration(string $class, array $methods = []): Migration
+    {
+        $migrationManager = $this->getMigrationManager();
+
+        $mock = $this->getMockBuilder(Migration::class)
+            ->setMockClassName($class)
+            ->setConstructorArgs([$migrationManager])
+            ->onlyMethods(array_keys($methods))
+            ->getMockForAbstractClass();
+
+        foreach ($methods as $name => $value) {
+            $mock
+                ->expects($this->any())
+                ->method($name)
+                ->willReturn($value);
+        }
+
+        return $mock;
+    }
+
+    /**
+     * @return MigrationManager
+     */
+    protected function getMigrationManager(): MigrationManager
+    {
+        return new MigrationManager(['alias' => '@twin/test/temp']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        (new Temp)->clear();
+    }
+}
