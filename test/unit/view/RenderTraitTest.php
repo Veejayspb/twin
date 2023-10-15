5c9655b1d300dfb9c90591465c9190c94cdc3bbc
Evgen <veejayspb@gmail.com>
2023-10-16 02:40:30 +0300

    [test]: RenderTraitTest.

---
+++ test/unit/view/RenderTraitTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use twin\test\helper\BaseTestCase;
+use twin\view\RenderTrait;
+
+class RenderTraitTest extends BaseTestCase
+{
+    public function testRenderPath()
+    {
+        $mock = $this->mock(RenderTrait::class); /* @var RenderTrait $mock */
+
+        $code = $this->catchExceptionCode(function () use ($mock) {
+            $mock->renderPath('@twin/test/notexists');
+        });
+
+        $this->assertSame(500, $code);
+
+        $content = null;
+
+        $code = $this->catchExceptionCode(function () use ($mock, &$content) {
+            $content = $mock->renderPath('@twin/test/helper/view/simple.php', ['text' => 'text']);
+        });
+
+        $this->assertSame(PHP_EOL . 'text', $content);
+        $this->assertSame(0, $code);
+    }
+}
