fdf2f1852594daf4de7962fdef745fe04e92ca5a
Evgen <veejayspb@gmail.com>
2024-05-21 05:34:41 +0300

    [test]: Json db model methods.

---
+++ test/helper/TestModel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace test\helper;
+
+use twin\model\Model;
+
+/**
+ * Class TestModel
+ * @property int $id
+ * @property string $name
+ */
+class TestModel extends Model
+{
+    /**
+     * @var array
+     */
+    public $_pk = ['id'];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function pkAttributes(): array
+    {
+        return $this->_pk;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName(): string
+    {
+        return 'test';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function attributeNames(): array
+    {
+        return [
+            'id',
+            'name',
+        ];
+    }
+}
