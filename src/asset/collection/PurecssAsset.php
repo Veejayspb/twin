35d9d9c39b8e6df7619d48222f717924edcce6f4
Evgen <veejayspb@gmail.com>
2025-09-30 13:24:34 +0300

    Asset: update purecss to 3.0.

--- src/asset/collection/PurecssAsset.php
+++ src/asset/collection/PurecssAsset.php
@@ -11,8 +11,8 @@ class PurecssAsset extends Asset
      * {@inheritdoc}
      */
     public array $css = [
-        'main' => 'https://cdn.jsdelivr.net/npm/purecss@2.1.0/build/pure-min.css',
-        'responsive' => 'https://cdn.jsdelivr.net/npm/purecss@2.1.0/build/grids-responsive-min.css',
+        'main' => 'https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css',
+        'responsive' => 'https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/grids-responsive-min.css',
     ];
 
     /**
@@ -21,7 +21,7 @@ class PurecssAsset extends Asset
     protected function prepareCss(Tag $tag, int|string $key): Tag
     {
         if ($key == 'main') {
-            $tag->integrity = 'sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH';
+            $tag->integrity = 'sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls';
             $tag->crossorigin = 'anonymous';
         }
         return $tag;
